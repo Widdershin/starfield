e87d34fbb8f765317a010ef319afd34ab0eb4c66
Nick Johnstone <ncwjohnstone@gmail.com>
2017-02-01 19:18:20 +1300

    Control speed with mouse

--- src/app.ts
+++ src/app.ts
@@ -7,30 +7,50 @@ function makeStarField (stars, scale, {width, height}) {
     .map(a => ({x: Math.random() * scale - width / 2, y: Math.random() * scale - height / 2}))
 }
 
-function makeStars (stars) {
-  return stars.concat(makeStarField(5, 0.001, {width: 1, height: 1}))
+function makeStars (state) {
+  const newStars = state.stars.concat(makeStarField(1, 0.001, {width: 1, height: 1}))
+
+  state.stars = newStars;
+
+  return state;
 }
 
-function updateStarField (stars, frame) {
-  return stars.map(star => {
+function setSpeed (state, speed) {
+  state.speed = speed / innerWidth;
+
+  state.speed = Math.max(state.speed, 0.001);
+
+  return state;
+}
+
+function updateStarField (state, frame) {
+  const newStars = state.stars.map(star => {;
     if (star.x > 0.5 || star.x < -0.5 || star.y > 0.5 || star.y < -0.5) {
       star.x = Math.random() * 0.001 - 0.0005;
       star.y = Math.random() * 0.001 - 0.0005;
     }
 
+    const speed = state.speed * 0.10;
+
     return {
-      x: star.x * 1.05,
-      y: star.y * 1.05
+      x: star.x * (1 + speed),
+      y: star.y * (1 + speed)
     }
   });
+
+  state.stars = newStars;
+
+  return state;
 }
 
-function renderStars (stars, {width, height}) {
-  return stars.map((star, index) => renderStar(star, index, {width, height}));
+function renderStars (stars, speed, {width, height}) {
+  return stars.map((star, index) => renderStar(star, index, speed, {width, height}));
 }
 
-function renderStar (star, key, {width, height}) {
-  const progress = 1 + Math.pow(Math.sqrt(Math.abs(star.x) / 0.5 * Math.abs(star.y) / 0.5) * 100, 1.1) / 100;
+function renderStar (star, key, speed, {width, height}) {
+  const progress = Math.pow(Math.sqrt(Math.abs(star.x) / 0.5 * Math.abs(star.y) / 0.5) * 100, 1.1) / 100;
+
+  const beamMultiplier = 1.001 + progress * speed;
 
   return (
     h('line', {
@@ -38,8 +58,8 @@ function renderStar (star, key, {width, height}) {
       attrs: {
         x1: width / 2 + star.x * width,
         y1: height / 2 + star.y * height,
-        x2: width / 2 + star.x * width * progress,
-        y2: height / 2 + star.y * height * progress,
+        x2: width / 2 + star.x * beamMultiplier * width,
+        y2: height / 2 + star.y * beamMultiplier * height,
         width: 1,
         stroke: 'white'
       }
@@ -51,26 +71,38 @@ function App (sources) {
   const frame$ = sources.Time
     .animationFrames()
 
-  const stars = [];
+  const initialState = {
+    stars: [],
+    speed: 1
+  };
 
-  const moveStars$ = frame$.map(frame => stars => updateStarField(stars, frame));
-  const makeStars$ = sources.Time.periodic(50).take(60).mapTo(makeStars);
+  const moveStars$ = frame$.map(frame => state => updateStarField(state, frame));
+  const makeStars$ = sources.Time.periodic(5).take(600).mapTo(makeStars);
+
+  const mouseMove$ = sources.DOM
+    .select('document')
+    .events('mousemove')
+    .compose(sources.Time.throttleAnimation)
+
+  const setSpeed$ = mouseMove$
+    .map(ev => state => setSpeed(state, ev.clientX));
 
   const reducer$ = xs.merge(
     moveStars$,
-    makeStars$
+    makeStars$,
+    setSpeed$
   );
 
-  const stars$ = reducer$.fold((stars, reducer: Function) => reducer(stars), stars);
+  const state$ = reducer$.fold((stars, reducer: Function) => reducer(stars), initialState);
 
   return {
-    DOM: stars$.map(stars => {
+    DOM: state$.map(state => {
       const width = innerWidth;
       const height = innerHeight;
 
       return (
         h('svg', {attrs: {width, height}}, [
-          ...renderStars(stars, {width, height})
+          ...renderStars(state.stars, state.speed, {width, height})
           //h('text', {attrs: {x: width / 2, y: height / 2, fill: 'white', 'font-family': 'Impact', 'font-size': 25, 'text-anchor': 'middle'}}, 'A journey through space and time')
         ])
       )
