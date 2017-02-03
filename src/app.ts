8f44b4b1dda3f90cbca1ac6d6f94e7cec75bd1df
Nick Johnstone <ncwjohnstone@gmail.com>
2017-02-03 16:30:38 +1300

    Replace movement with easing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import {div, button, svg, h} from '@cycle/dom';
 import xs from 'xstream';
 import * as keycode from 'keycode';
-
+import * as eases from 'eases';
 
 function currentSlide (state) {
   return state.slides[state.currentSlide];
@@ -9,9 +9,11 @@ function currentSlide (state) {
 
 function nextSlide (state) {
   state.currentSlide += 1;
-  state.targetPosition = currentSlide(state).position - 1;
-  state.speed = 0.01;
+  state.targetPosition = currentSlide(state).position - 0.5;
   state.tweenStartPosition = state.currentPosition;
+  state.tweenStartTime = state.time;
+  const distance = state.targetPosition - state.currentPosition;
+  state.tweenEndTime = state.time + distance * 10;
   return state;
 }
 
@@ -19,14 +21,6 @@ function makeStarField (stars, scale, {width, height}) {
   return new Array(stars)
     .fill(0)
     .map(a => ({x: Math.random() * scale - width / 2, y: Math.random() * scale - height / 2}))
-}
-
-function makeStars (state) {
-  const newStars = state.stars.concat(makeStarField(1, 0.001, {width: 1, height: 1}))
-
-  state.stars = newStars;
-
-  return state;
 }
 
 function setSpeed (state, speed) {
@@ -37,36 +31,45 @@ function setSpeed (state, speed) {
   return state;
 }
 
+
 function updateStarField (state, frame) {
-  state.currentPosition += state.speed;
+  state.time += 16;
+
+  if (state.stars.length < 300 && state.stars.length < Math.floor(state.currentPosition * 2)) {
+    const starsToMake = Math.floor(state.currentPosition * 2 - state.stars.length);
+
+    state.stars = state.stars.concat(makeStarField(starsToMake, 0.001, {width: 1, height: 1}))
+  }
+
+  let oldPosition = state.currentPosition;
 
   const totalDistance = state.targetPosition - state.tweenStartPosition;
   const distanceToPosition = state.targetPosition - state.currentPosition;
 
-  if (distanceToPosition < 0) {
-    state.speed /= 1.1;
-  } else if (distanceToPosition < totalDistance / 2) {
-    state.speed /= 1.04;
-  } else if (state.currentPosition < state.targetPosition) {
-    state.speed *= 1.04;
+  if (state.currentPosition < state.targetPosition) {
+    const animationTime = state.time - state.tweenStartTime;
+    const progressRatio = animationTime / (state.tweenEndTime - state.tweenStartTime);
+    const tweenDistance = state.targetPosition - state.tweenStartPosition;
+
+    state.currentPosition = state.tweenStartPosition + tweenDistance * eases.quintInOut(progressRatio);
   }
 
+  state.currentPosition += 0.0001;
+  state.speed = Math.max(0.001, state.currentPosition - oldPosition);
 
-  const newStars = state.stars.map(star => {;
+  const speed = state.speed * 0.10;
+
+  for (let i = 0; i < state.stars.length; i++) {
+    const star = state.stars[i];
+
     if (star.x > 0.5 || star.x < -0.5 || star.y > 0.5 || star.y < -0.5) {
       star.x = Math.random() * 0.001 - 0.0005;
       star.y = Math.random() * 0.001 - 0.0005;
     }
 
-    const speed = state.speed * 0.10;
-
-    return {
-      x: star.x * (1 + speed),
-      y: star.y * (1 + speed)
-    }
-  });
-
-  state.stars = newStars;
+    star.x *= (1 + speed);
+    star.y *= (1 + speed);
+  }
 
   return state;
 }
@@ -123,6 +126,23 @@ function renderSlide (slide, position, {width, height}) {
     }, slide.text)
   );
 }
+const slides = [
+  '',
+  'My Cycle.js story',
+  'A journey through space and time',
+  'In the beginning',
+  'One day I watched a talk',
+  'And then I built cycle-time-travel',
+  "But it wasn't good enough",
+  "Thankfully Staltz",
+  "Still very confused",
+  "What about hot reloading?",
+  "hmr api works but resets state",
+  "cycle-restart cometh!",
+  "now time travel is possible",
+  "need to tame time"
+
+].map((text, i) => ({position: i === 0 ? 0 : 500 + i * 200, text}))
 
 function App (sources) {
   const frame$ = sources.Time
@@ -131,39 +151,15 @@ function App (sources) {
   const initialState = {
     stars: [],
     speed: 0,
+    time: 0,
     currentPosition: 0,
     targetPosition: 0,
     tweenStartPosition: 0,
     currentSlide: 0,
-    slides: [
-      {
-        position: 0,
-        text: ''
-      },
-
-      {
-        position: 100,
-        text: 'Hello Universe'
-      },
-
-      {
-        position: 200,
-        text: 'A journey through space and time'
-      },
-
-      {
-        position: 500,
-        text: 'Another slide for fun'
-      }
-    ]
+    slides
   };
 
   const moveStars$ = frame$.map(frame => state => updateStarField(state, frame));
-
-  const mouseMove$ = sources.DOM
-    .select('document')
-    .events('mousemove')
-    .compose(sources.Time.throttleAnimation)
 
   const nextSlide$ = sources.DOM
     .select('document')
@@ -171,14 +167,8 @@ function App (sources) {
     .filter(ev => ev.code === 'Space')
     .mapTo(nextSlide);
 
-  const makeStars$ = nextSlide$
-    .take(1)
-    .map(() => sources.Time.periodic(5).take(600).mapTo(makeStars))
-    .flatten();
-
   const reducer$ = xs.merge(
     moveStars$,
-    makeStars$,
     nextSlide$
   );
 
